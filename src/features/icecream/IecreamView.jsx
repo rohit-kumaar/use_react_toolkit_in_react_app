797a1416c7e8ea048d042b04734b89d29aa158cc
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-05-12 16:15:14 +0530

    Initial commit

---
+++ src/features/icecream/IecreamView.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function IcecreamView() {
+  return (
+    <div>
+        <h2>Number of Ice creams - </h2>
+        <button>Order Ice cream</button>
+        <button>Restock Ice creams</button>
+    </div>
+  )
+}
+
+export default IcecreamView
---
+++ src/features/user/UserView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserView() {
+  return (
+    <div>
+        <h2>Lists of users</h2>
+    </div>
+  )
+}
+
+export default UserView
