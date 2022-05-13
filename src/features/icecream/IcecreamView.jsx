b607c095b05d407bb088cd93411fcce22a320c3d
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-05-13 10:41:37 +0530

    Apply useDispatch

--- src/features/icecream/IcecreamView.jsx
+++ src/features/icecream/IcecreamView.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { ordered, restocked } from "./icecreamSlice";
 
 function IcecreamView() {
   const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
+  const dispatch = useDispatch();
   return (
     <div>
       <h2>Number of Ice creams - {numOfIcecreams}</h2>
-      <button>Order Ice cream</button>
-      <button>Restock Ice creams</button>
+      <button onClick={() => dispatch(ordered())}>Order Ice cream</button>
+      <button onClick={() => dispatch(restocked(2))}>Restock Ice creams</button>
     </div>
   );
 }
